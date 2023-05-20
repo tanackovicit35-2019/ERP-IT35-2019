67f27c7d9bf2d09c373adfd0d51f271a1e30851f
tanackovicit35-2019 <73586484+tanackovicit35-2019@users.noreply.github.com>
2023-05-20 23:35:45 +0200

    Delete BiletarnicaBack directory

--- BiletarnicaBack/Models/LoginResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BiletarnicaBack.Models
-{
-    public class LoginResponseDto
-    {
-        public int korisnikID { get; set; }
-        public string role { get; set; }
-        public string token { get; set; }
-    }
-}
